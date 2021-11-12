ba2335a9282335e52282ee93f3bb2a55f9536984
Dan Anderson <dan@vshift.com>
2021-11-12 11:34:33 -0500

    Enable lint on file buffer change instead of save

--- linter.py
+++ linter.py
@@ -2,8 +2,7 @@ from SublimeLinter.lint import Linter, STREAM_STDOUT
 
 
 class CSpell(Linter):
-    cmd = 'cspell ${file_on_disk}'
+    cmd = 'cspell stdin'
     defaults = {'selector': 'source'}
-    regex = r'^(?P<filename>[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
-    tempfile_suffix = '-'
+    regex = r'^[^:]*:(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
     error_stream = STREAM_STDOUT
