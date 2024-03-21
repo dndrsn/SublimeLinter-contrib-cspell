d2cd7c07afff45effe3b94e1ef8728a639ff0024
Dan Anderson <dan@vshift.com>
2024-03-21 15:37:24 -0400

    update default type to warning

--- linter.py
+++ linter.py
@@ -1,4 +1,4 @@
-from SublimeLinter.lint import Linter, STREAM_STDOUT
+from SublimeLinter.lint import Linter, STREAM_STDOUT, WARNING
 
 
 class CSpell(Linter):
@@ -6,3 +6,4 @@ class CSpell(Linter):
     defaults = {'selector': 'source'}
     regex = r'^[^:]*:(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
     error_stream = STREAM_STDOUT
+    default_type = WARNING
